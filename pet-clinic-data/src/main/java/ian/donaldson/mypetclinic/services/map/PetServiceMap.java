3932c3d5bc0a3c068eb314fed1fde86cf1024d45
IanMDonaldson <imdonaldson@ualr.edu>
2022-01-31 23:05:24 -0600

    added barebones vets page and controller

---
+++ pet-clinic-data/src/main/java/ian/donaldson/mypetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package ian.donaldson.mypetclinic.services.map;
+
+import ian.donaldson.mypetclinic.model.Pet;
+import ian.donaldson.mypetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        delete(pet);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/ian/donaldson/mypetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package ian.donaldson.mypetclinic.services.map;
+
+import ian.donaldson.mypetclinic.model.Vet;
+import ian.donaldson.mypetclinic.model.Vet;
+import ian.donaldson.mypetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        delete(vet);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
